7939a25ce0e842e7a24597392f751a4bca90fb5e
en4395 <ellathenoyes@gmail.com>
2023-06-20 09:17:34 -0400

    Got rid of bag of carrots, baby carrots :( , and disco ball.

--- src/main/java/workshop/setup/Registration.java
+++ src/main/java/workshop/setup/Registration.java
@@ -26,17 +26,10 @@ public class Registration {
     }
 
     //Create RegistryObjects for blocks
-    public static final RegistryObject<Block> BAG_OF_CARROTS_BLOCK = BLOCKS.register("bag_of_carrots_block",()->new Block(BlockBehaviour.Properties.of(Material.DIRT).strength(6f).requiresCorrectToolForDrops()));
-    public static final RegistryObject<Item> BAG_OF_CARROTS_BLOCK_ITEM = fromBlock(BAG_OF_CARROTS_BLOCK);
-
-    public static final RegistryObject<Block> DISCO_BALL_BLOCK = BLOCKS.register("disco_ball_block", ()->new Block(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops().lightLevel(blockstate->14)));
-    public static final RegistryObject<Item> DISCO_BALL_BLOCK_ITEM = fromBlock(DISCO_BALL_BLOCK);
-
     public static final RegistryObject<Block> SPECIAL_MUSHROOM_BLOCK = BLOCKS.register("special_mushroom_block", ()->new Block(BlockBehaviour.Properties.of(Material.STONE).strength(4f).requiresCorrectToolForDrops().noOcclusion()));
     public static final RegistryObject<Item> SPECIAL_MUSHROOM_BLOCK_ITEM = fromBlock(SPECIAL_MUSHROOM_BLOCK);
 
     //Create RegistryObjects for items
-    public static final RegistryObject <Item> BABY_CARROT_ITEM = ITEMS.register("baby_carrot_item", ()->new Item(new Item.Properties()));
 
     //Create item from existing block RegistryObject
     public static <B extends Block> RegistryObject<Item> fromBlock(RegistryObject<B> block) {
